dfcc6f3f049928ab4f4c78cefbc8b7c246a5aa8f
Petr Stukalov <petr@polkapad.network>
2023-02-15 20:56:54 -0300

    init

--- sa.tf
+++ sa.tf
@@ -4,7 +4,8 @@ resource "kubernetes_service_account" "this" {
     name        = var.irsa_sa
     namespace   = var.irsa_namespace
     annotations = {
-      "eks.amazonaws.com/role-arn" = module.this.arn
+      "eks.amazonaws.com/role-arn"         = module.this.arn
+      "kubernetes.io/service-account.name" = var.irsa_sa
     }
     type = "kubernetes.io/service-account-token"
   }
